5e719d153b3d74997b20c2af2a6cc7177972dd30
giseok <hgsok3056@naver.com>
2019-10-13 22:24:07 +0900

    191013

---
+++ 01.기본 알고리즘/src/chap02_반복/Q10_직각이등변삼각형.java
@@ -0,0 +1,11 @@
+package chap02_반복;
+
+public class Q10_직각이등변삼각형 {
+
+	public static void main(String[] args) {
+		// 연습문제 Q15   40p
+		// 왼쪽 위, 오른쪽 위, 오른쪽 아래가 직각인 이등변 삼각형을 출력하는 메서드 작성하기
+
+	} // end main
+
+} // end class
