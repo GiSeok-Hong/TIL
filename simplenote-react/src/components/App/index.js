487dbbe40dbce499d0c10f0f6101433b2ffc8c4a
giseok <hgsok3056@naver.com>
2019-10-19 19:02:42 +0900

    state로 데이터 관리하기

--- simplenote-react/src/components/App/index.js
+++ simplenote-react/src/components/App/index.js
@@ -6,15 +6,30 @@ import List from '../List';
 import Note from '../Note';
 
 class App extends React.Component {
+  // state : UI의 상태를 기록하는 데이터. 일반적으로 버튼의 활성화 상태, input의 입력 값,
+  //         체크박스 체크 여부 등의 상태를 관리     
+  // state 선언
+  //  notes: 앱의 노트 정보를 담은 array
+  //  activeId: 리스트에서 유저가 선택하여 활성화 된 노트의 id
+  state = {
+    notes: [],
+    activeId: null,
+  }
+
   render(){
+    const { notes, activeId } = this.state;
     return (
       <div className="app">
         <Header />
         <div className="container">
-          <List />
+          {/* note와 activeId props로 전달 */}
+          <List notes={notes} activeId={activeID} />
           <Note />
         </div>
       </div>
     );
   }
 }
+
+// 아래 코드가 있어야 컴파일 됨?
+export default App;
--- simplenote-react/src/index.js
+++ simplenote-react/src/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { unregister } from './registerServiceWorker'
+import * as serviceWorker from './serviceWorker';
+
+// import { unregister } from './registerServiceWorker'  이것도 오래된 거라 안됨
 
 ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
+
+// unregister(); 오래된 코드라 이거 쓰면 작동이 안됨
+serviceWorker.unregister();
