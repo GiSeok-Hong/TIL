69d45d94239efd92ebed7f67a4c07ddaa380d9af
giseok <hgsok3056@naver.com>
2020-02-07 17:19:11 +0900

    연습: 1-9 array

---
+++ JavaScript/NomadCoder_momonton/1-9/index.js
@@ -0,0 +1,16 @@
+// Array
+const monday = "Mon";
+const tuesday = "Tue";
+const wednesday = "Wed";
+const thursday = "Thu";;
+const friday = "Fri";
+const saturday = "Sat";
+const sunday = "Sun";
+
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+console.log(daysOfWeek);  // 
+console.log(daysOfWeek[2]); // Wed
+
+
+// Object
