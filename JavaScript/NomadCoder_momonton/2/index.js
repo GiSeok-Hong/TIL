27d1eb0b68dcaf8dca43e899a6fd0e78f98e359b
giseok <hgsok3056@naver.com>
2020-02-08 16:38:15 +0900

    연습: 2-1 first JS Function

---
+++ JavaScript/NomadCoder_momonton/2/index.js
@@ -0,0 +1,10 @@
+// console.log("hello hong");
+// console.log("hello park");
+// console.log("hello kim");
+// console.log("hello world");
+
+function sayHello(name, age){
+  console.log('Hello!', name, "hahaha", age);
+};
+
+sayHello("hong", 20);
