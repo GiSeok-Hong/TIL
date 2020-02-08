5a214cfdbca0236f965ffb267e71f75fcc3059e5
giseok <hgsok3056@naver.com>
2020-02-08 20:20:54 +0900

    연습 2-7 function practice2

--- JavaScript/NomadCoder_momonton/2/index.js
+++ JavaScript/NomadCoder_momonton/2/index.js
@@ -1,42 +1,17 @@
 const title = document.querySelector("#title");
-
-function handleResize(){
-  console.log("I have been resized ~!")
-}
-
-// window.addEventListener("resize", handleResize());  호출을 하지 않았는데 실행이 됨
-// window 크기가 바뀌면 handleResize 함수를 실행한다.
-window.addEventListener("resize", handleResize);
-console.dir(title);
-
-
-
-const BASE_COLOR = "blue";
-const CHANGE_COLOR = "red";
+const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-  const currentColor = title.style.color;
-  if(currentColor === BASE_COLOR){
-    title.style.color = CHANGE_COLOR;
-    console.log("change color")
-  } else {
-    title.style.color = BASE_COLOR;
-    console.log("change")
-  }
+  // const hasClass = title.classList.contains(CLICKED_CLASS);
+  // if(!hasClass){
+  //   title.classList.add(CLICKED_CLASS);
+  // } else {
+  //   title.classList.remove(CLICKED_CLASS);
+  // }
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init(){
-  title.style.color= BASE_COLOR;
-  title.addEventListener("click", handleClick);
+  title.addEventListener("click", handleClick)
 }
 init();
-
-function handleOffline(){
-  console.log("offline");
-}
-
-function handleOnline(){
-  console.log("online");
-}
-window.addEventListener("online", handleOnline);
-window.addEventListener("offline", handleOffline);
