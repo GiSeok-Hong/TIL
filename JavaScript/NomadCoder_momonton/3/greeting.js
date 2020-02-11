feaa8797d6a228b21a36dd7f95c84622178b8a88
giseok <hgsok3056@naver.com>
2020-02-11 17:05:06 +0900

    연습: 3-4 Saving the User Name part two

--- JavaScript/NomadCoder_momonton/3/greeting.js
+++ JavaScript/NomadCoder_momonton/3/greeting.js
@@ -5,6 +5,23 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser",
       SHOWING_CN = "showing";
 
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // 기본 이벤트를 막음. enter 입력시 적어둔 값이 사라지지 않고 유지하기 위해
+  const currentValue = input.value;
+  // console.log(currentValue);
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName(){
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit)
+}
+
 function paintGreeting(text){
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,10 +31,10 @@ function paintGreeting(text){
 function loadName(){
   const currentUser = localStorage.getItem(USER_LS);
   if(currentUser === null){
-    console.log("undefined 유저가 없습니다")
+    askForName();
   } else {
     paintGreeting(currentUser);
-    console.log("")
+    
   }
 }
 
